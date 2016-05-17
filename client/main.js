b38f9f11f57cc4f1181ef8977c3305b8dcb26599
Florian Mettetal <florian.mettetal@gmail.com>
2016-05-17 11:42:00 -0700

    incomplete timer

---
+++ client/main.js
@@ -0,0 +1,27 @@
+// import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
+//
+// import './main.html';
+
+Template.timer.onCreated(()=> {
+  // counter starts at 0
+  self = Template.instance();
+  self.timer = new ReactiveVar(0);
+  Meteor.setInterval(function() {
+    let currValue = self.timer.get();
+    console.log("curr> ", currValue);
+    currValue = currValue++;
+    self.timer.set(currValue);
+  }, 1000);
+});
+
+Template.timer.helpers({
+  time:()=> {
+    return Template.instance().timer.get();
+  },
+});
+
+Template.timer.events({
+
+
+});
